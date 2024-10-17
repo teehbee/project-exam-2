01d299c823399474a947389dbba4519f24fead8a
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-17 14:56:34 +0200

    add overlay to frontpage tiles

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -16,7 +16,7 @@ function Home() {
           <div className="row">
             <div className="col-12 col-lg-6">
               <h1 className="text-light secondary-font fs-2rem-to-3rem pt-lg-5">Spend Quality Holidays With Us</h1>
-              <p className="text-light fw-light">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+              <p className="text-light fs-1rem-to-1-25rem">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
             </div>
             <div className="col-12 col-lg-6">
               <SearchForm />
@@ -27,25 +27,53 @@ function Home() {
       <section className="container text-center">
         <h2 className="py-5 secondary-font fs-1-5rem-to-2-5rem">Find your next getaway</h2>
         <div className="row pb-5 g-1">
-          <div className="col-6 col-lg-3">
-            <Link to="venue">
-              <img className="img-fluid" src={placeHolderImage} alt="" />
-            </Link>
+          <div className="col-6 col-lg-3 fp-img-container">
+            <div className="position-relative">
+              <Link to="venue">
+                <img className="img-fluid" src={placeHolderImage} alt="" />
+                <div className="fp-img-overlay">
+                  <div className="fp-img-overlay-text text-start ps-3">
+                    <p className="secondary-font text-light">Name of accommodation</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className="col-6 col-lg-3">
-            <Link to="venue">
-              <img className="img-fluid" src={placeHolderImage} alt="" />
-            </Link>
+          <div className="col-6 col-lg-3 fp-img-container">
+            <div className="position-relative">
+              <Link to="venue">
+                <img className="img-fluid" src={placeHolderImage} alt="" />
+                <div className="fp-img-overlay">
+                  <div className="fp-img-overlay-text text-start ps-3">
+                    <p className="secondary-font text-light">Name of accommodation</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className="col-6 col-lg-3">
-            <Link to="venue">
-              <img className="img-fluid" src={placeHolderImage} alt="" />
-            </Link>
+          <div className="col-6 col-lg-3 fp-img-container">
+            <div className="position-relative">
+              <Link to="venue">
+                <img className="img-fluid" src={placeHolderImage} alt="" />
+                <div className="fp-img-overlay">
+                  <div className="fp-img-overlay-text text-start ps-3">
+                    <p className="secondary-font text-light">Name of accommodation</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className="col-6 col-lg-3">
-            <Link to="venue">
-              <img className="img-fluid" src={placeHolderImage} alt="" />
-            </Link>
+          <div className="col-6 col-lg-3 fp-img-container">
+            <div className="position-relative">
+              <Link to="venue">
+                <img className="img-fluid" src={placeHolderImage} alt="" />
+                <div className="fp-img-overlay">
+                  <div className="fp-img-overlay-text text-start ps-3">
+                    <p className="secondary-font text-light">Name of accommodation</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
         <div className="text-end">
