141dd111700477f94c2064636657406a959e3069
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-17 08:32:38 +0200

    corrections to form and hero image

--- src/components/forms/SearchForm.tsx
+++ src/components/forms/SearchForm.tsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 const schema = yup.object().shape({
-  venueSearchLocation: yup.string().required("Location is required"),
-  venueSearchStartDate: yup.date().required("Start date is required"),
-  venueSearchEndDate: yup.date().required("End date is required"),
-  venueSearchNrGuests: yup.number().required("Number of guests is required"),
+  venueSearchName: yup.string().required("Search field cannot be empty"),
 });
 
 function SearchForm() {
-  const [isSubmitted, setIsSubmitted] = useState(false);
   const {
     register,
     handleSubmit,
@@ -20,32 +15,23 @@ function SearchForm() {
     resolver: yupResolver(schema),
   });
 
-  function onSubmit(data) {
+  function onSubmit(data: string) {
     console.log(data);
-    setIsSubmitted(true);
   }
 
   return (
     <div className="bg-dark-gray-color text-light search-form mx-auto px-4 py-5">
-      <h2 className="secondary-font py-3 fs-1-5rem-to-2rem">Book accommodation</h2>
+      <h2 className="secondary-font py-3 fs-1-5rem-to-2rem">Search for accommodation</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column">
-        <label htmlFor="venueSearchLocation">Location</label>
-        <input type="text" id="venueSearchLocation" placeholder="Fagernes" {...register("venueSearchLocation")} className="mb-2"/>
-        {errors.venueSearchLocation && <p className="text-danger">{errors.venueSearchLocation.message}</p>}
-        <label htmlFor="venueSearchStartDate">Arrival date</label>
-        <input type="date" value="2024-10-16" id="venueSearchStartDate" {...register("venueSearchStartDate")} className="mb-2"/>
-        {errors.venueSearchStartDate && <p className="text-danger">{errors.venueSearchStartDate.message}</p>}
-        <label htmlFor="venueSearchEndDate">Checkout date</label>
-        <input type="date" value="2024-10-16" id="venueSearchEndDate" {...register("venueSearchEndDate")} className="mb-2"/>
-       {errors.venueSearchEndDate && <p className="text-danger">{errors.venueSearchEndDate.message}</p>}
-        <p className="fw-light d-none">Number of nights</p>
-        <label htmlFor="venueSearchNrGuests">Number of guests</label>
-        <input type="number" id="venueSearchNrGuests" {...register("venueSearchNrGuests")} placeholder="1" min="0" max="100" className="mb-2"/>
-        {errors.venueSearchNrGuests && <p className="text-danger">{errors.venueSearchNrGuests.message}</p>}
-        <button className="main-button-red py-2 mt-3 fs-1-125rem">SEARCH</button>
+        <label htmlFor="venueSearchName" className="py-2">
+          Enter search text here
+        </label>
+        <input type="text" id="venueSearchName" placeholder="Fagernes" {...register("venueSearchName")} className="mb-3" />
+        {errors.venueSearchName && <p className="text-danger m-0">{errors.venueSearchName.message}</p>}
+        <button className="main-button-red py-2 mt-2 mt-md-4 fs-1-125rem">SEARCH</button>
       </form>
     </div>
-  )
+  );
 }
 
 export default SearchForm;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,25 +2,28 @@ import { frontpageHeroImageSmall, frontpageHeroImageLarge } from "../assets/img"
 import SearchForm from "../components/forms";
 
 function Home() {
-  return(
+  return (
     <>
-      <picture >
-      <source media="(min-width: 992px)" srcSet={frontpageHeroImageLarge} />
-      <img className="position-absolute top-0 banner-img" src={frontpageHeroImageSmall} aria-label="A beautiful fjord" />
+      <picture>
+        <source media="(min-width: 992px)" srcSet={frontpageHeroImageLarge} />
+        <img className="position-absolute top-0 banner-img" src={frontpageHeroImageSmall} aria-label="A beautiful fjord" />
       </picture>
-    <div className="container py-5">
-      <div className="row">
-        <div className="col-12 col-lg-6 pt-lg-5">
-          <h1 className="text-light secondary-font fs-2rem-to-3rem">Spend Quality Holidays With Us</h1>
-          <p className="text-light pt-3 fw-light">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
-        </div>
-        <div className="col-12 col-lg-6 pt-3">
-          <SearchForm />
+      <div className="hero-container d-flex align-items-center justify-content-center position-relative">
+        <div className="container">
+          <div className="row">
+            <div className="col-12 col-lg-6">
+              <h1 className="text-light secondary-font fs-2rem-to-3rem">Spend Quality Holidays With Us</h1>
+              <p className="text-light fw-light">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+            </div>
+            <div className="col-12 col-lg-6">
+              <SearchForm />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+      <div></div>
     </>
-  )
+  );
 }
 
 export default Home;
