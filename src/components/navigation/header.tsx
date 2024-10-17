3d5e53b4d74c78188e79ae21e4c4d1233df87453
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-17 11:12:32 +0200

    correcting nav

--- src/components/navigation/header.tsx
+++ src/components/navigation/header.tsx
@@ -3,10 +3,32 @@ import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { logoSmall, logoLarge } from "../../assets/logo";
 import profileIcon from "../../assets/icon";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const [dropDownOpen, setIsDropDownOpen] = useState(false);
+
+  const isTransparent = location.pathname === "/" || location.pathname === "venues";
+
+  // Makes sure the header is transparent on homepage and venue page only
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 992) {
+        setIsDropDownOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  // Dynamically changes the header color to dark when dropdown is open
+
   return (
-    <header className="position-relative">
+    <header className={`position-relative ${isTransparent && !dropDownOpen ? "" : "bg-dark-gray-color"}`}>
       <div className="main-nav container">
         <div className="upper-nav d-none d-md-flex py-2 ps-0 justify-content-between border-bottom-white">
           <ul className="d-md-flex flex-row d-none ps-0">
@@ -31,16 +53,16 @@ function Header() {
             </li>
           </ul>
         </div>
-        <Navbar expand="lg" className="py-3">
+        <Navbar expand="lg" className={`py-3 ${isTransparent && !dropDownOpen ? "" : "bg-dark-gray-color"}`}>
           <Link to="/">
             <picture className="cursor-pointer">
               <source media="(min-width: 992px)" srcSet={logoLarge} />
               <img src={logoSmall} aria-label="main logo" />
             </picture>
           </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsDropDownOpen(!dropDownOpen)} />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto align-items-center">
+            <Nav className="dropdown-active ms-auto align-items-center">
               <Link className="text-light fs-1-125rem fw-light nav-link" to="/">
                 HOME
               </Link>
