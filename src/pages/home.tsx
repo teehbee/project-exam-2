f51fdf2df5182d01390fc93e79ec981903ec8435
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-19 11:35:55 +0200

    move frontpage tiles to separate component

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,8 @@
-import { frontpageHeroImageSmall, frontpageHeroImageLarge } from "../assets/img";
-import SearchForm from "../components/forms";
-import placeHolderImage from "../assets/placeholderImg";
-import { frontPageImage1, frontPageImage2 } from "../assets/frontpageImg";
 import { Link } from "react-router-dom";
+import FrontpageTiles from "../components/frontpageContent/FrontpageTiles";
+import { frontpageHeroImageSmall, frontpageHeroImageLarge } from "../assets/img";
+import placeHolderImage from "../assets/placeholderImg";
+import SearchForm from "../components/forms";
 
 function Home() {
   return (
@@ -82,36 +82,7 @@ function Home() {
           </Link>
         </div>
       </section>
-      <section className="container my-5">
-        <div className="row mb-4">
-          <div className="col-12 col-lg-6">
-            <img className="img-fluid" src={frontPageImage1} alt="" />
-          </div>
-          <div className="col-12 col-lg-6 text-center text-lg-start">
-            <h3 className="secondary-font fs-1-5rem-to-2-5rem pt-5">Earn money on your property</h3>
-            <p className="fw-light pt-3">
-              Looking to earn extra income from your property? With Holidaze, it's easy to turn your space into a profitable vacation rental! Our user-friendly platform allows you to list your property online in just a few simple steps. Whether you own a cozy apartment, a luxurious villa, or a charming cottage, Holidaze connects you with travelers searching for unique accommodations. You have full control over pricing, availability, and booking conditions, while our secure system ensures hassle-free payments. Join Holidaze today and start welcoming guests to your property with confidence!
-            </p>
-            <Link to="register" className="dark-font pt-3">
-              Register here!
-            </Link>
-          </div>
-        </div>
-        <div className="row flex-lg-row-reverse">
-          <div className="col-12 col-lg-6">
-            <img className="img-fluid" src={frontPageImage2} alt="" />
-          </div>
-          <div className="col-12 col-lg-6 text-center text-lg-end">
-            <h3 className="secondary-font fs-1-5rem-to-2-5rem pt-5">Find your next getaway</h3>
-            <p className="fw-light pt-3">
-              Looking to earn extra income from your property? With Holidaze, it's easy to turn your space into a profitable vacation rental! Our user-friendly platform allows you to list your property online in just a few simple steps. Whether you own a cozy apartment, a luxurious villa, or a charming cottage, Holidaze connects you with travelers searching for unique accommodations. You have full control over pricing, availability, and booking conditions, while our secure system ensures hassle-free payments. Join Holidaze today and start welcoming guests to your property with confidence!
-            </p>
-            <Link to="register" className="dark-font pt-3">
-              Register here!
-            </Link>
-          </div>
-        </div>
-      </section>
+      <FrontpageTiles />
     </>
   );
 }
