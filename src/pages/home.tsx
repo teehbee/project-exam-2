0ce06d38df7a2c138ee0ee576c5fbc1ca6474d60
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-21 07:37:32 +0200

    correction to text on frontpage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,8 +13,8 @@ function Home() {
         <div className="container">
           <div className="row">
             <div className="col-12 col-lg-6">
-              <h1 className="text-light secondary-font fs-2rem-to-3rem pt-lg-5 pe-md-5">Spend Quality Holidays With Us</h1>
-              <p className="text-light fs-1rem-to-1-25rem">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>
+              <h1 className="text-light secondary-font fs-2rem-to-3rem pt-lg-5 pe-md-5 me-md-5">Spend Quality Holidays With Us</h1>
+              <p className="text-light fs-1rem-to-1-25rem">Find your perfect getaway with our diverse accommodations. Whether for relaxation or celebration, we offer spaces that feel like home. Start creating unforgettable moments today!</p>
             </div>
             <div className="col-12 col-lg-6">
               <SearchForm />
