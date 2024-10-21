863af54fc0c900c239b77da1b9382caf7d22d914
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-21 09:19:50 +0200

    add ts validation for data using SubmitHandler

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import { Link } from "react-router-dom";
 import Spinner from "react-bootstrap/Spinner";
 
+const schema = yup.object().shape({
+  loginEmail: yup.string().email("Please enter a valid email address").required("Email is required"),
+  loginPassword: yup.string().required("Password is required"),
+});
+
+interface LoginFormInputs {
+  loginEmail: string;
+  loginPassword: string;
+}
+
 function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<LoginFormInputs> = (data) => {
+    console.log(data);
+  };
+
   const id = React.useId();
 
   return (
@@ -11,24 +36,25 @@ function LoginForm() {
         <div className="col-12 col-md-6 mx-auto mt-5">
           <div className="custom-border-gray text-start p-3">
             <h1 className="fs-1-5rem-to-2rem fw-light">Login</h1>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group d-flex flex-column">
                 <label htmlFor={id + "-loginEmail"} className="mt-2 fs-0-75rem-to-1rem fw-light">
                   Email
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fw-light fs-0-75rem-to-0-875rem" type="email" placeholder="iamawesome@stud.noroff.no" name="login-email" id={id + "-loginEmail"} />
-                <p className="d-none pt-1 m-0 text-danger fs-0-75rem-to-0-875rem">Incorrect email address</p>
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fw-light fs-0-75rem-to-0-875rem" type="email" placeholder="iamawesome@stud.noroff.no" id={id + "-loginEmail"} {...register("loginEmail")} />
+                {errors.loginEmail && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.loginEmail.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
                 <label htmlFor={id + "-loginPassword"} className="mt-2 fs-0-75rem-to-1rem fw-light">
                   Password
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fw-light fs-0-75rem-to-0-875rem" type="password" placeholder="********" name="login-password" id={id + "-loginPassword"} />
-                <p className="d-none pt-1 m-0 text-danger fs-0-75rem-to-0-875rem">Password is incorrect</p>
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fw-light fs-0-75rem-to-0-875rem" type="password" placeholder="********" id={id + "-loginPassword"} {...register("loginPassword")} />
+                {errors.loginPassword && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.loginPassword.message}</p>}
               </div>
               <button className="main-button-gray mt-4 p-1 p-md-2">
                 Login <Spinner className="d-none ms-1" animation="border" size="sm" variant="light" />
               </button>
+              <p className="d-none pt-1 m-0 text-danger fs-0-75rem-to-0-875rem">Incorrect email address or password</p>
               <div className="mt-2 mt-md-3">
                 <p className="fw-light fs-0-75rem-to-1rem">
                   Not registered?{" "}
