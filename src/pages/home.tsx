4e873764c0039205fcc7b8b0d4ef9c0090fca96d
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-04 09:21:17 +0100

    corrections to frontpage banner

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -12,8 +12,8 @@ function Home() {
         <source media="(min-width: 992px)" srcSet={frontpageHeroImageLarge} />
         <img className="position-absolute top-0 banner-img" src={frontpageHeroImageSmall} aria-label="A beautiful fjord" />
       </picture>
-      <section className="hero-container">
-        <div className="container test position-absolute start-50 top-50 translate-middle">
+      <section className="hero-container position-relative d-flex justify-content-center align-items-center">
+        <div className="container mt-5 pt-5">
           <div className="row">
             <div className="col-10 col-lg-6 mx-auto text-center text-lg-start">
               <h1 className="text-light secondary-font fs-2rem-to-3rem pt-md-5 mt-md-5 pb-3 pe-lg-5">Spend Quality Holidays With Us</h1>
