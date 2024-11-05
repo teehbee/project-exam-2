6cb6507f8191dbbe2e5e762e0c2b740021681ee6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-05 09:53:58 +0100

    set up and style not found page

--- src/components/error/NotFound.tsx
+++ src/components/error/NotFound.tsx
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 function PageNotFound() {
-  return <div>Page not found</div>;
+  return (
+    <div className="container text-center py-5">
+      <h1 className="secondary-font fw-bold py-3">404 Not Found</h1>
+      <p className="pb-3">The page you are looking for might have been removed, had its name changed or is temporary unavailable</p>
+      <Link className="font-gray" to="/">
+        Return back to homepage
+      </Link>
+    </div>
+  );
 }
 
 export default PageNotFound;
