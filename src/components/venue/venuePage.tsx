589a2e661d3e4a607532362ca9a2b1af7ab6dce6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-05 12:56:18 +0100

    add margin-bottom to single venue page

--- src/components/venue/venuePage.tsx
+++ src/components/venue/venuePage.tsx
@@ -2,7 +2,7 @@ import { VenueImageAndText, VenueBooking } from "./elements";
 
 function SingleVenuePage() {
   return (
-    <div className="container mt-5">
+    <div className="container my-5">
       <div className="row text-center">
         <VenueImageAndText />
         <VenueBooking />
