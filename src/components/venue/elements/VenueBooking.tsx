b06fe4bc9663d57a14a50d604ad5e85d90921361
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-05 12:55:36 +0100

    set up and complete form for choosing number of guests

--- src/components/venue/elements/VenueBooking.tsx
+++ src/components/venue/elements/VenueBooking.tsx
@@ -1,10 +1,13 @@
 import { BookingCalendar, BookingNumberOfGuests } from "./";
 
 function VenueBooking() {
+  const handleGuestsChange = (numberOfGuests: number) => {
+    console.log("Number of guests:", numberOfGuests);
+  };
   return (
     <div className="col-12 col-md-7">
       <BookingCalendar />
-      <BookingNumberOfGuests />
+      <BookingNumberOfGuests onGuestsChange={handleGuestsChange} />
     </div>
   );
 }
