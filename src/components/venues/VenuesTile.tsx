255bcd9ef8fd4f9af42a5e16c06ca9e831d838d6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-05 10:31:19 +0100

    add facilities icons to venues page

--- src/components/venues/VenuesTile.tsx
+++ src/components/venues/VenuesTile.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { petIcon, wifiIcon, restaurantIcon, parkingIcon } from "../../assets/icon";
 import placeHolderImage from "../../assets/placeholderImg";
 
 function VenueTile() {
@@ -13,7 +14,13 @@ function VenueTile() {
             <Link className="text-decoration-none" to="/venue">
               <p className="mb-1 secondary-font fs-1rem-to-1-5rem">Name of accommodation</p>
             </Link>
-            <p className="fs-0-75rem-to-1rem">Location</p>
+            <p className="fs-0-75rem-to-1rem mb-0">Location</p>
+            <div className="mt-md-2">
+              <img className="facility-icon me-2" src={wifiIcon} alt="" />
+              <img className="facility-icon me-2" src={restaurantIcon} alt="" />
+              <img className="facility-icon me-2 " src={parkingIcon} alt="" />
+              <img className="facility-icon me-2" src={petIcon} alt="" />
+            </div>
           </div>
           <div className="d-flex justify-content-between align-items-center">
             <div className="d-flex flex-column">
@@ -22,7 +29,7 @@ function VenueTile() {
             </div>
             <div className="align-items-end">
               <Link to="/venue">
-                <button className="main-button-red">Book now</button>
+                <button className="main-button-red">Book</button>
               </Link>
             </div>
           </div>
