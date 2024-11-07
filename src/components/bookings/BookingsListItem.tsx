ae16ee100e53c37ccf561f630669f68a8653b4af
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-07 16:32:58 +0100

    add styling to bookingslistitems

--- src/components/bookings/BookingsListItem.tsx
+++ src/components/bookings/BookingsListItem.tsx
@@ -2,14 +2,14 @@ function BookingsListItem() {
   return (
     <div className="d-flex justify-content-between pt-3 border-bottom-gray">
       <div>
-        <p className="mb-1">Name of renter - Email address</p>
+        <p className="mb-1 fs-0-75rem-to-0-875rem">Name of renter - Email address</p>
       </div>
       <div className="d-flex">
         <div className="pe-2">
-          <p className="mb-1">From date - To date</p>
+          <p className="mb-1 fs-0-75rem-to-0-875rem">From date - To date</p>
         </div>
         <div>
-          <p className="mb-1">Number of guests</p>
+          <p className="mb-1 fs-0-75rem-to-0-875rem">Number of guests</p>
         </div>
       </div>
     </div>
