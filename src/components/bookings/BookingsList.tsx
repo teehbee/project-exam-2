cb29d31b219787eb12bc363da2cac928f5230640
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-07 11:57:38 +0100

    start adding elements to bookings list

--- src/components/bookings/BookingsList.tsx
+++ src/components/bookings/BookingsList.tsx
@@ -1,5 +1,16 @@
 function BookingsList() {
-  return <div className="col-12 col-md-6">List goes here</div>;
+  return (
+    <div className="col-12 col-md-6">
+      <h2 className="secondary-font">Bookings</h2>
+      <ul className="list-group list-group-flush text-start py-5">
+        <li className="list-group-item">An item span</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </div>
+  );
 }
 
 export default BookingsList;
