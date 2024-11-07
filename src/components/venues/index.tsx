9969ddf548111e28c12270ef4e14213f4293a979
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-07 09:10:18 +0100

    move venues page into component

--- src/components/venues/index.tsx
+++ src/components/venues/index.tsx
@@ -2,5 +2,6 @@ import VenueTile from "./VenuesTile";
 import VenueFiltering from "./filter";
 import FilterButton from "./FilterButton";
 import FilterForm from "./FilterForm";
+import VenuesPage from "./Venues";
 
-export { VenueTile, VenueFiltering, FilterButton, FilterForm };
+export { VenueTile, VenueFiltering, FilterButton, FilterForm, VenuesPage };
--- src/pages/venues.tsx
+++ src/pages/venues.tsx
@@ -1,48 +1,9 @@
-import { venueHeroImageLarge, venueHeroImageSmall } from "../assets/img";
-import { SearchFormMain } from "../components/forms";
-import { VenueTile, VenueFiltering } from "../components/venues";
+import { VenuesPage } from "../components/venues";
 import { useScrollToTop } from "../components/utils";
-import MainLoader from "../components/loader";
 
 function Venues() {
   useScrollToTop();
-  return (
-    <>
-      <picture>
-        <source media="(min-width: 992px)" srcSet={venueHeroImageLarge} />
-        <img className="position-absolute top-0 banner-img" src={venueHeroImageSmall} aria-label="A beautiful fjord" />
-      </picture>
-      <section className="hero-container position-relative d-flex justify-content-center align-items-center">
-        <div className="container ">
-          <SearchFormMain />
-        </div>
-      </section>
-      <section id="venues-list" className="container py-5 pt-md-0 my-5">
-        <div className="py-3 py-lg-5">
-          <p className="secondary-font fs-1rem-to-2rem mb-1">0 venues match your search</p>
-          <p className="cursor-pointer fs-0-75rem-to-1rem">Show all venues</p>
-          <VenueFiltering />
-        </div>
-
-        <div className="text-center py-5 d-none">
-          <MainLoader />
-        </div>
-        <div className="row g-3">
-          <VenueTile />
-          <VenueTile />
-          <VenueTile />
-          <VenueTile />
-          <VenueTile />
-          <VenueTile />
-          <VenueTile />
-          <VenueTile />
-        </div>
-        <div className="text-center pt-5">
-          <p className="secondary-font fs-1rem-to-1-25rem cursor-pointer">Load more venues...</p>
-        </div>
-      </section>
-    </>
-  );
+  return <VenuesPage />;
 }
 
 export default Venues;
