9292f359bdc28cd7b858f18981d6d575ce623914
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-11 13:44:16 +0100

    add 4 venues to frontpage

--- src/components/frontpageContent/FrontpageVenuePreview.tsx
+++ src/components/frontpageContent/FrontpageVenuePreview.tsx
@@ -1,68 +1,36 @@
 import { Link } from "react-router-dom";
-import { placeHolderImage } from "../../assets/placeholderImg";
 import { useApi } from "../api";
 import { Venue } from "../api/interfaces";
 import { VENUES_ENDPOINT } from "../api/const";
 
 function FrontpageVenuePreview() {
-  const { data, error, loading } = useApi<Venue[]>(VENUES_ENDPOINT, "GET", null, false);
+  const { data, error, loading } = useApi<{ data: Venue[] }>(VENUES_ENDPOINT, "GET", null, false);
 
   console.log("Fetched Data:", data);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
+
+  const venues = data?.data || [];
+
   return (
     <section className="container text-center mt-125px mt-md-4">
-      <h2 className=" pb-5 py-md-5 secondary-font fs-1-5rem-to-2-5rem">Find your next getaway</h2>
+      <h2 className="pb-5 py-md-5 secondary-font fs-1-5rem-to-2-5rem">Find your next getaway</h2>
       <div className="row pb-5 gy-4">
-        <div className="col-6 col-lg-3 fp-img-container">
-          <div className="position-relative">
-            <Link to="venue">
-              <img className="img-fluid form-box-shadow-no-br" src={placeHolderImage} alt="placeholder image" />
-              <div className="fp-img-overlay">
-                <div className="fp-img-overlay-text text-start ps-3">
-                  <p className="secondary-font text-light">Name of accommodation</p>
+        {venues.slice(0, 4).map((venue) => (
+          <div key={venue.id} className="col-6 col-lg-3 fp-img-container">
+            <div className="position-relative">
+              <Link to={`venue/${venue.id}`}>
+                <img className="img-fluid form-box-shadow-no-br" src={venue.media[0]?.url} alt={venue.media[0]?.alt || "Venue image"} />
+                <div className="fp-img-overlay">
+                  <div className="fp-img-overlay-text text-start ps-3">
+                    <p className="secondary-font text-light">{venue.name}</p>
+                  </div>
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="col-6 col-lg-3 fp-img-container">
-          <div className="position-relative">
-            <Link to="venue">
-              <img className="img-fluid form-box-shadow-no-br" src={placeHolderImage} alt="placeholder image" />
-              <div className="fp-img-overlay">
-                <div className="fp-img-overlay-text text-start ps-3">
-                  <p className="secondary-font text-light">Name of accommodation</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-6 col-lg-3 fp-img-container">
-          <div className="position-relative">
-            <Link to="venue">
-              <img className="img-fluid form-box-shadow-no-br" src={placeHolderImage} alt="placeholder image" />
-              <div className="fp-img-overlay">
-                <div className="fp-img-overlay-text text-start ps-3">
-                  <p className="secondary-font text-light">Name of accommodation</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="col-6 col-lg-3 fp-img-container">
-          <div className="position-relative">
-            <Link to="venue">
-              <img className="img-fluid form-box-shadow-no-br" src={placeHolderImage} alt="placeholder image" />
-              <div className="fp-img-overlay">
-                <div className="fp-img-overlay-text text-start ps-3">
-                  <p className="secondary-font text-light">Name of accommodation</p>
-                </div>
-              </div>
-            </Link>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="text-end">
         <Link className="text-decoration-none" to="venues#venues-list">
