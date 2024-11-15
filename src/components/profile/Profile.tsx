0bacc15da7d2a6a8618aa4ec41bf7e11a1a1f726
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-15 13:12:04 +0100

    add 404 not found for error handling on profile page

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -3,6 +3,7 @@ import { useApi } from "../api";
 import { getProfileEndpoint } from "../api/const/variables";
 import { VenueResponse } from "../api/interfaces";
 import { FrontPageLoader } from "../frontpageContent";
+import PageNotFound from "../error";
 
 function ProfilePage() {
   const name = localStorage.getItem("name");
@@ -14,7 +15,7 @@ function ProfilePage() {
   console.log("Fetched Data:", profileData);
 
   if (loading) return <FrontPageLoader />;
-  if (error) return;
+  if (error) return <PageNotFound />;
 
   return (
     <div className="container row mx-auto text-center py-5">
