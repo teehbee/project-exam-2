02576dd9661157ad6cfebec3b4ab4f23c3ff9d3a
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-15 15:02:55 +0100

    change styling for avatar

--- src/components/profile/ProfileBio.tsx
+++ src/components/profile/ProfileBio.tsx
@@ -6,7 +6,7 @@ interface ProfileBio {
 
 const ProfileBio: React.FC<ProfileBio> = ({ bio }) => {
   return (
-    <div className=" border">
+    <div>
       <h2 className="fs-0-75rem-to-1rem py-3">Biography</h2>
       <p className="fs-0-75rem-to-1rem fw-light">{bio ? bio : "No bio text yet, please update profile to add bio. "}</p>
       <Link to="/update-profile">
