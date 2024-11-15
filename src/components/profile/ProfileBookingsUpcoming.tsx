bcc7df2d1d5352a6332e56f073be7e2e58b52949
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-15 16:15:41 +0100

    add images to upcoming bookings on profile

--- src/components/profile/ProfileBookingsUpcoming.tsx
+++ src/components/profile/ProfileBookingsUpcoming.tsx
@@ -1,23 +1,30 @@
 import { placeHolderImage } from "../../assets/placeholderImg";
 import { Link } from "react-router-dom";
 import { ProfileBookings } from "../api/interfaces";
+import { formatDate } from "../utils";
 
 interface bookingData {
   booking: ProfileBookings;
 }
 
 const ProfileBookingTile: React.FC<bookingData> = ({ booking }) => {
+  // Variables
+  const img = booking.venue.media.url;
   const name = booking.venue.name;
   const city = booking.venue.location.city;
   const country = booking.venue.location.country;
   const dateFrom = booking.dateFrom;
   const dateTo = booking.dateTo;
+  // Formatted dates
+  const formattedDateFrom = formatDate(dateFrom);
+  const formattedDateTo = formatDate(dateTo);
+
   console.log("booking", booking);
   return (
     <div className="profile-booking-container form-box-shadow mt-3 row mx-1 mx-md-0">
       <div className="col-4 col-md-3 px-0">
         <Link to="/venue">
-          <img className="form-box-shadow" src={placeHolderImage} aria-label="placeholder" />
+          <img className="form-box-shadow" src={img} aria-label="placeholder" />
         </Link>
       </div>
       <div className="col-8 col-md-9 text-start ps-3 ps-md-5">
@@ -26,7 +33,7 @@ const ProfileBookingTile: React.FC<bookingData> = ({ booking }) => {
           {city ? city : "No location added"}, {country ? country : ""}
         </p>
         <p className="fw-light fs-0-625rem-to-1rem mb-0 mt-md-4">
-          {dateFrom} to {dateTo}
+          {formattedDateFrom} to {formattedDateTo}
         </p>
         <p className="fw-light fs-0-625rem-to-1rem mb-0 mt-md-2">Number of guests</p>
         <p className="fs-0-625rem-to-1rem mt-md-2">Total sum</p>
--- src/components/utils/index.tsx
+++ src/components/utils/index.tsx
@@ -2,5 +2,6 @@ import useRedirectTimer from "./useRedirectTimer";
 import { getTodaysDate, getTomorrowsDate } from "./dateUtils";
 import useScrollToTop from "./useScrollToTop";
 import { checkForFilterChanges, setInitialFilterValues } from "./filterVenuesUtils";
+import formatDate from "./useFormatDate";
 
-export { useRedirectTimer, getTodaysDate, getTomorrowsDate, useScrollToTop, checkForFilterChanges, setInitialFilterValues };
+export { useRedirectTimer, getTodaysDate, getTomorrowsDate, useScrollToTop, checkForFilterChanges, setInitialFilterValues, formatDate };
