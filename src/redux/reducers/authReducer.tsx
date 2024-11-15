763b70b66b43b5ca26805087e6168b07b3239285
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-15 08:11:30 +0100

    add reducer for login and logout

---
+++ src/redux/reducers/authReducer.tsx
@@ -0,0 +1,31 @@
+import { LOGIN, LOGOUT, AuthActionTypes } from "../types/authActionTypes";
+
+interface AuthState {
+  token: string | null;
+  name: string | null;
+  loggedIn: boolean;
+}
+
+const initialState: AuthState = {
+  token: null,
+  name: null,
+  loggedIn: false,
+};
+
+const authReducer = (state = initialState, action: AuthActionTypes): AuthState => {
+  switch (action.type) {
+    case LOGIN:
+      return {
+        ...state,
+        token: action.payload.token,
+        name: action.payload.name,
+        loggedIn: true,
+      };
+    case LOGOUT:
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
--- src/redux/types/authActionTypes.tsx
+++ src/redux/types/authActionTypes.tsx
@@ -1,2 +1,16 @@
 export const LOGIN = "LOGIN";
 export const LOGOUT = "LOGOUT";
+
+interface LoginAction {
+  type: typeof LOGIN;
+  payload: {
+    token: string;
+    name: string;
+  };
+}
+
+interface LogoutAction {
+  type: typeof LOGOUT;
+}
+
+export type AuthActionTypes = LoginAction | LogoutAction;
