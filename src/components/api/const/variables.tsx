b4e6ad51b81611c87eb1c06c769c03c7c4d348f1
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-15 12:56:49 +0100

    add endpoint with dynamically added name from localstorage for fetching profile data

--- src/components/api/const/variables.tsx
+++ src/components/api/const/variables.tsx
@@ -6,3 +6,9 @@ export const REGISTER_ENDPOINT = "auth/register";
 // venues
 
 export const VENUES_ENDPOINT = "holidaze/venues?_bookings=true&_owner=true";
+
+// profile (dynamically adding name from localStorage)
+
+export const getProfileEndpoint = (name: string) => {
+  return `holidaze/profiles/${name}?_bookings=true&_venues=true`;
+};
