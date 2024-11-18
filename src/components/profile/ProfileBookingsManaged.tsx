7bd9dbb965988e8c518dc18f8a1d1b334b351b7f
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-18 08:10:32 +0100

    add images and alt text to managed venue list

--- src/components/profile/ProfileBookingsManaged.tsx
+++ src/components/profile/ProfileBookingsManaged.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { placeHolderImage } from "../../assets/placeholderImg";
 import { VenueManagerBookings } from "../api/interfaces";
 
 interface VenueManagerBookingsData {
@@ -9,11 +8,13 @@ interface VenueManagerBookingsData {
 const ProfileBookingsManaged: React.FC<VenueManagerBookingsData> = ({ venue }) => {
   const id = venue.id;
   const name = venue.name;
+  const img = venue.media.length > 0 ? venue.media[0].url : "";
+  const alt = venue.media.length > 0 ? venue.media[0].alt : "Accommodation image";
   return (
     <div className="profile-booking-container form-box-shadow mt-3 row mx-1 mx-md-0">
       <div className="col-4 col-md-3 px-0">
         <Link to={`/venue/${id}`}>
-          <img className="form-box-shadow" src={placeHolderImage} aria-label="placeholder" />
+          <img className="form-box-shadow" src={img} alt={alt} />
         </Link>
       </div>
       <div className="col-8 col-md-9 text-start ps-3 ps-md-5 py-3 d-flex justify-content-between">
