8e0beca5a7f3d5c6e93948e4a3d284532f703eca
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-18 10:28:58 +0100

    fetch id on single venue using useParams()

--- src/components/venue/venuePage.tsx
+++ src/components/venue/venuePage.tsx
@@ -1,8 +1,11 @@
 import { VenueImageAndText, VenueBooking } from "./elements";
 import { useParams } from "react-router-dom";
-import { useApi } from "../api";
+// import { VENUE_ENDPOINT } from "../api/const/variables";
+// import { useApi } from "../api";
 
 const SingleVenuePage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  console.log(id);
   return (
     <div className="container my-5">
       <div className="row text-center">
