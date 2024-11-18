a3baad45efd6bd0795a0c95f644a20fe5c2ac29b
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-18 07:27:48 +0100

    add dynamic name to url when entering profile update from profile, to be used for updating profile

--- src/components/profile/ProfileBio.tsx
+++ src/components/profile/ProfileBio.tsx
@@ -2,14 +2,15 @@ import { Link } from "react-router-dom";
 
 interface ProfileBio {
   bio: string;
+  name: string;
 }
 
-const ProfileBio: React.FC<ProfileBio> = ({ bio }) => {
+const ProfileBio: React.FC<ProfileBio> = ({ bio, name }) => {
   return (
     <div>
       <h2 className="fs-0-75rem-to-1rem py-3">Biography</h2>
       <p className="fs-0-75rem-to-1rem fw-light">{bio ? bio : "No bio text yet, please update profile to add bio. "}</p>
-      <Link to="/update-profile">
+      <Link to={`/update-profile/${name}`}>
         <button className="main-button-gray fs-0-75rem-to-1rem my-3 w-50">Update profile</button>
       </Link>
     </div>
