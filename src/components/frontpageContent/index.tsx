e72fdb070939de440caa8335eebb63309c957d40
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-18 10:04:03 +0100

    move homepage content to Frontpage.tsx

--- src/components/frontpageContent/index.tsx
+++ src/components/frontpageContent/index.tsx
@@ -2,5 +2,6 @@ import FrontpageTiles from "./FrontpageTiles";
 import FrontpageVenuePreview from "./FrontpageVenuePreview";
 import FrontPageLoader from "./FrontpageLoader";
 import FrontPageError from "./FrontPageError";
+import Frontpage from "./Frontpage";
 
-export { FrontpageTiles, FrontpageVenuePreview, FrontPageLoader, FrontPageError };
+export { FrontpageTiles, FrontpageVenuePreview, FrontPageLoader, FrontPageError, Frontpage };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,34 +1,9 @@
-import { FrontpageTiles, FrontpageVenuePreview } from "../components/frontpageContent";
-import { frontpageHeroImageSmall, frontpageHeroImageLarge } from "../assets/img";
-import { SearchForm } from "../components/forms";
 import { useScrollToTop } from "../components/utils";
+import { Frontpage } from "../components/frontpageContent";
 
 function Home() {
   useScrollToTop();
-
-  return (
-    <>
-      <picture>
-        <source media="(min-width: 992px)" srcSet={frontpageHeroImageLarge} />
-        <img className="position-absolute top-0 banner-img" src={frontpageHeroImageSmall} aria-label="A beautiful fjord" />
-      </picture>
-      <section className="hero-container position-relative d-flex justify-content-center align-items-center">
-        <div className="container mt-5 pt-5">
-          <div className="row">
-            <div className="col-10 col-lg-6 mx-auto text-center text-lg-start">
-              <h1 className="text-light secondary-font fs-2rem-to-3rem pt-md-5 mt-md-5 pb-3 pe-lg-5">Spend Quality Holidays With Us</h1>
-              <p className="text-light fs-1rem-to-1-25rem d-none d-lg-block">Find your perfect getaway with our diverse accommodations. Whether for relaxation or celebration, we offer spaces that feel like home. Start creating unforgettable moments today!</p>
-            </div>
-            <div className="col-12 col-lg-6">
-              <SearchForm />
-            </div>
-          </div>
-        </div>
-      </section>
-      <FrontpageVenuePreview />
-      <FrontpageTiles />
-    </>
-  );
+  return <Frontpage />;
 }
 
 export default Home;
