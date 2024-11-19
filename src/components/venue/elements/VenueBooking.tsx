e306e830ef7750582fc96e419d1dbc7fb581534b
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-19 15:17:44 +0100

    set up dateHandler in parent booking component

--- src/components/venue/elements/VenueBooking.tsx
+++ src/components/venue/elements/VenueBooking.tsx
@@ -10,13 +10,22 @@ const VenueBooking: React.FC<SingleVenueProp> = ({ venue }) => {
   const [fromDate, setFromDate] = useState<Date | null>(null);
   const [toDate, setToDate] = useState<Date | null>(null);
 
+  // Handler for dates from calendar
+
+  const handleDateSelection = (fromDate: Date, toDate: Date) => {
+    setFromDate(fromDate);
+    setToDate(toDate);
+    console.log("From date:", fromDate);
+    console.log("To date", toDate);
+  };
+
   // handling of change in number of guests to be displayed in booking sum
   const handleGuestsChange = (newNumberOfGuests: number) => {
     setNumberOfGuests(newNumberOfGuests);
   };
   return (
     <div className="col-12 col-md-7">
-      <BookingCalendar venue={venue} />
+      <BookingCalendar venue={venue} onDateSelect={handleDateSelection} />
       <BookingNumberOfGuests onGuestsChange={handleGuestsChange} />
       <BookingSum numberOfGuests={numberOfGuests} />
     </div>
