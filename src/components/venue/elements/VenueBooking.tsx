43ffa0b9053b2a54318d0f59b9d07d83d7af62e6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-19 15:31:05 +0100

    revert back to earlier stage

--- src/components/venue/elements/VenueBooking.tsx
+++ src/components/venue/elements/VenueBooking.tsx
@@ -7,17 +7,6 @@ const VenueBooking: React.FC<SingleVenueProp> = ({ venue }) => {
   console.log("id is", venue.data.id);
   // state for number of guests
   const [numberOfGuests, setNumberOfGuests] = useState(1);
-  const [fromDate, setFromDate] = useState<Date | null>(null);
-  const [toDate, setToDate] = useState<Date | null>(null);
-
-  // Handler for dates from calendar
-
-  const handleDateSelection = (fromDate: Date, toDate: Date) => {
-    setFromDate(fromDate);
-    setToDate(toDate);
-    console.log("From date:", fromDate);
-    console.log("To date", toDate);
-  };
 
   // handling of change in number of guests to be displayed in booking sum
   const handleGuestsChange = (newNumberOfGuests: number) => {
@@ -25,7 +14,7 @@ const VenueBooking: React.FC<SingleVenueProp> = ({ venue }) => {
   };
   return (
     <div className="col-12 col-md-7">
-      <BookingCalendar venue={venue} onDateSelect={handleDateSelection} />
+      <BookingCalendar venue={venue} />
       <BookingNumberOfGuests onGuestsChange={handleGuestsChange} />
       <BookingSum numberOfGuests={numberOfGuests} />
     </div>
