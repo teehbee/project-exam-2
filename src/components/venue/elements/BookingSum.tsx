92315b0c4184606c2efdf68136b4e6375ede6370
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-21 09:45:54 +0100

    set up variables needed for sending booking date to the api and import useApi to booking page

--- src/components/venue/elements/BookingSum.tsx
+++ src/components/venue/elements/BookingSum.tsx
@@ -10,7 +10,9 @@ function BookingSum({ numberOfGuests, nights, sum }: BookingSumProps) {
       <h3 className="fs-1-25rem-to-1-5rem pb-3">Price of stay: NOK {sum},-</h3>
       <p className="fs-1rem-to-1-25rem fw-light">Number of nights: {nights}</p>
       <p className="fs-1rem-to-1-25rem pb-3 fw-light">Number of people: {numberOfGuests}</p>
-      <button className="main-button-red w-100">BOOK NOW</button>
+      <button className="main-button-red w-100" onClick={onBooking}>
+        BOOK NOW
+      </button>
     </div>
   );
 }
--- src/components/venue/elements/VenueBooking.tsx
+++ src/components/venue/elements/VenueBooking.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { SingleVenueProp } from "../../api/const/interfaces";
 import { BookingCalendar, BookingNumberOfGuests, BookingSum } from "./";
 import { formatDate } from "../../utils";
+import { useApi } from "../../api";
 import { useCalculateTotalCost } from "../../utils";
 
 const VenueBooking: React.FC<SingleVenueProp> = ({ venue }) => {
@@ -10,30 +11,37 @@ const VenueBooking: React.FC<SingleVenueProp> = ({ venue }) => {
   const [fromDate, setFromDate] = useState<string | null>(null);
   const [toDate, setToDate] = useState<string | null>(null);
 
+  const venueId = venue.data.id;
   const venuePrice = venue.data.price;
 
   const formattedFromDate = fromDate ? formatDate(fromDate) : null;
   const formattedToDate = toDate ? formatDate(toDate) : null;
   const { totalCost, numberOfNights } = useCalculateTotalCost(formattedFromDate || "", formattedToDate || "", venuePrice);
 
+  console.log(venueId);
+  console.log(formattedFromDate);
+  console.log(formattedToDate);
+  console.log(numberOfGuests);
+
   const handleBookingDates = (from: string | null, to: string | null) => {
     setFromDate(from);
     setToDate(to);
   };
 
-  // console.log("From Date:", fromDate);
-  // console.log("To Date:", toDate);
-  // console.log("nights", numberOfNights);
-
   // handling of change in number of guests to be displayed in booking sum
   const handleGuestsChange = (newNumberOfGuests: number) => {
     setNumberOfGuests(newNumberOfGuests);
   };
+
+  const handleBooking() {
+
+  }
+
   return (
     <div className="col-12 col-md-7">
       <BookingCalendar venue={venue} onDateChange={handleBookingDates} />
       <BookingNumberOfGuests onGuestsChange={handleGuestsChange} />
-      <BookingSum numberOfGuests={numberOfGuests} sum={totalCost} nights={numberOfNights} />
+      <BookingSum numberOfGuests={numberOfGuests} sum={totalCost} nights={numberOfNights} onBooking={handleBooking} />
     </div>
   );
 };
