457897ab40d7aa88cf672560d452fe0dcf3a3600
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-21 14:44:00 +0100

    more changes to font-weights on upcoming bookings

--- src/components/profile/ProfileBookingsUpcoming.tsx
+++ src/components/profile/ProfileBookingsUpcoming.tsx
@@ -40,9 +40,9 @@ const ProfileBookingTile: React.FC<bookingData> = ({ booking }) => {
       </div>
       <div className="col-8 col-md-9 text-start ps-3 ps-md-5 pt-1">
         <Link className="text-decoration-none font-gray" to={`/venue/${id}`}>
-          <h4 className="secondary-font fs-1rem-to-1-5rem mt-1 mb-0">{name}</h4>
+          <h4 className="secondary-font fs-1rem-to-1-5rem mt-1 mb-0 fw-bold">{name}</h4>
         </Link>
-        <p className="fs-0-625rem-to-1rem mt-md-1">
+        <p className="fs-0-625rem-to-1rem mt-md-1 fw-medium">
           {city} {country}
         </p>
         <p className="fs-0-625rem-to-1rem mb-0 mt-md-4">
