e9c26132102e597557c9f1efb3775e42c77ddf32
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-22 14:25:20 +0100

    create and import endpoint for creating venue

--- src/components/api/const/variables.tsx
+++ src/components/api/const/variables.tsx
@@ -1,15 +1,19 @@
-// authorization
+// authorization (POST)
 
 export const LOGIN_ENDPOINT = "auth/login";
 export const REGISTER_ENDPOINT = "auth/register";
 
-// venues
+// venues (GET)
 
 export const VENUES_ENDPOINT = "holidaze/venues?_bookings=true&_owner=true&sort=created";
 
-// single venue
+// single venue (GET)
 
 export const getVenueEndpoint = (id: string) => `holidaze/venues/${id}?_bookings=true`;
+
+// create venue (POST)
+
+export const CREATE_VENUE_ENDPOINT = "holidaze/venues";
 
 // profile (dynamically adding name from localStorage)
 
@@ -17,6 +21,6 @@ export const getProfileEndpoint = (name: string): string => {
   return `holidaze/profiles/${name}?_bookings=true&_venues=true`;
 };
 
-// booking
+// booking (POST)
 
 export const BOOK_VENUE_ENDPOINT = "holidaze/bookings";
