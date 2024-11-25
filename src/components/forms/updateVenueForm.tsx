d42676ff0801b0d2b38291435cb167f1a32ee762
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-25 10:25:13 +0100

    create regular ids for form inputs and lables for updating venue to not conflict on the useParams id

--- src/components/forms/updateVenueForm.tsx
+++ src/components/forms/updateVenueForm.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useNavigate } from "react-router-dom";
@@ -10,10 +10,13 @@ import { createVenueSchema } from "./schemas";
 // Types for rental form message
 
 function UpdateVenueForm() {
-  // State for displaying loader in submit button
   const [loginLoader, setLoginLoader] = useState(false);
-  // For navigating after login
   const navigate = useNavigate();
+
+  const { id } = useParams<{ id: string }>();
+
+  console.log("id is", id);
+
   // Form validation including default values for checkboxes
   const {
     register,
@@ -59,7 +62,6 @@ function UpdateVenueForm() {
   // };
 
   // useId for setting unique id to form inputs
-  const id = React.useId();
 
   return (
     <div className="container">
@@ -74,84 +76,84 @@ function UpdateVenueForm() {
             <h1 className="fs-1-5rem-to-2rem">Update venue</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-name"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateName" className="mt-2 fs-0-75rem-to-1rem">
                   Name<span className="text-danger">*</span>
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="Enter name of venue here" id={id + "-name"} {...register("name")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="Enter name of venue here" id="updateName" {...register("name")} />
                 {errors.name && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.name.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-city"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateCity" className="mt-2 fs-0-75rem-to-1rem">
                   City<span className="text-danger">*</span>
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="E.g. Fagernes" id={id + "-city"} {...register("location.city")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="E.g. Fagernes" id="updateCity" {...register("location.city")} />
                 {errors.location?.city && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.location.city.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-country"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateCountry" className="mt-2 fs-0-75rem-to-1rem">
                   Country<span className="text-danger">*</span>
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="E.g. Norway" id={id + "-country"} {...register("location.country")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="E.g. Norway" id="updateCountry" {...register("location.country")} />
                 {errors.location?.country && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.location.country.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-description"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateDescription" className="mt-2 fs-0-75rem-to-1rem">
                   Description<span className="text-danger">*</span>
                 </label>
-                <textarea className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" placeholder="Description of venue" rows={4} id={id + "-description"} {...register("description")} />
+                <textarea className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" placeholder="Description of venue" rows={4} id="updateDescription" {...register("description")} />
                 {errors.description && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.description.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-url"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateUrl" className="mt-2 fs-0-75rem-to-1rem">
                   Image<span className="text-danger">*</span>
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="url" placeholder="E.g. https://image.com" id={id + "-url"} {...register("media.0.url")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="url" placeholder="E.g. https://image.com" id="updateUrl" {...register("media.0.url")} />
                 {errors.media?.[0]?.url && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.media[0].url.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-alt"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateAlt" className="mt-2 fs-0-75rem-to-1rem">
                   Image description
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="E.g. Beautiful sunset in Valdres" id={id + "-alt"} {...register("media.0.alt")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="text" placeholder="E.g. Beautiful sunset in Valdres" id="updateAlt" {...register("media.0.alt")} />
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-price"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updatePrice" className="mt-2 fs-0-75rem-to-1rem">
                   Price per night in NOK<span className="text-danger">*</span>
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="number" placeholder="E.g. 1000" id={id + "-price"} {...register("price")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="number" placeholder="E.g. 1000" id="updatePrice" {...register("price")} />
                 {errors.price && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.price.message}</p>}
               </div>
               <div className="form-group d-flex flex-column">
-                <label htmlFor={id + "-maxGuests"} className="mt-2 fs-0-75rem-to-1rem">
+                <label htmlFor="updateGuests" className="mt-2 fs-0-75rem-to-1rem">
                   Max number of guests<span className="text-danger">*</span>
                 </label>
-                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="number" placeholder="E.g. 1" id={id + "-maxGuests"} {...register("maxGuests")} />
+                <input className="mt-1 custom-border-gray text-ident-5px p-1 p-md-2 form-input-bg fs-0-75rem-to-0-875rem" type="number" placeholder="E.g. 1" id="updateGuests" {...register("maxGuests")} />
                 {errors.maxGuests && <p className="text-danger fs-0-75rem-to-0-875rem pt-1">{errors.maxGuests.message}</p>}
               </div>
               <div className="ms-1 row pt-3 pt-md-4 pb-3">
                 <div className="col-6 form-check d-flex align-items-center">
-                  <input className="form-check-input cursor-pointer" type="checkbox" value="" id={id + "-wifi"} {...register("wifi")} />
-                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor={id + "-wifi"}>
+                  <input className="form-check-input cursor-pointer" type="checkbox" value="" id="updateWifi" {...register("wifi")} />
+                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor="updateWifi">
                     Wifi
                   </label>
                 </div>
                 <div className="col-6 form-check d-flex align-items-center">
-                  <input className="form-check-input cursor-pointer" type="checkbox" value="" id={id + "-parking"} />
-                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor={id + "-parking"} {...register("parking")}>
+                  <input className="form-check-input cursor-pointer" type="checkbox" value="" id="updateParking" />
+                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor="updateParking" {...register("parking")}>
                     Parking
                   </label>
                 </div>
               </div>
               <div className="ms-1 row pb-2 justify-content-center w-100">
                 <div className="col-6 form-check d-flex align-items-center">
-                  <input className="cursor-pointer form-check-input" type="checkbox" value="" id={id + "-breakfast"} />
-                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor={id + "-breakfast"} {...register("breakfast")}>
+                  <input className="cursor-pointer form-check-input" type="checkbox" value="" id="updateBreakfast" />
+                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor="updateBreakfast" {...register("breakfast")}>
                     Breakfast
                   </label>
                 </div>
                 <div className="col-6 form-check d-flex align-items-center">
-                  <input className="cursor-pointer form-check-input" type="checkbox" value="" id={id + "-pets"} {...register("pets")} />
-                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor={id + "-pets"}>
+                  <input className="cursor-pointer form-check-input" type="checkbox" value="" id="updatePets" {...register("pets")} />
+                  <label className="cursor-pointer form-check-label mt-1 ps-1 fs-0-75rem-to-1rem" htmlFor="updatePets">
                     Pets allowed
                   </label>
                 </div>
