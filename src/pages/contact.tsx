4fb25b222484e376614e5aee3f5e080fd8f69402
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-30 09:25:12 +0100

    modify single venue component to send data to parent component for use in helmet

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,18 @@
 import { ContactForm } from "../components/forms";
 import { useScrollToTop } from "../components/utils";
+import { Helmet } from "react-helmet";
 
 function Contact() {
   useScrollToTop();
-  return <ContactForm />;
+  return (
+    <>
+      <Helmet>
+        <title>Contact Us - Holidaze</title>
+        <meta name="description" content="Have questions or need assistance? Reach out to the Holidaze team for support. We're here to help with any inquiries or issues you may have regarding your bookings or listings." />
+      </Helmet>
+      <ContactForm />
+    </>
+  );
 }
 
 export default Contact;
