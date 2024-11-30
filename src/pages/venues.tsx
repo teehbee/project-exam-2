d413587fb98a25e793e24f4b38aee64252029e66
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-30 09:55:11 +0100

    add meta title and descriptions to several pages

--- src/pages/venues.tsx
+++ src/pages/venues.tsx
@@ -1,9 +1,18 @@
 import { VenuesPage } from "../components/venues";
 import { useScrollToTop } from "../components/utils";
+import { Helmet } from "react-helmet";
 
 function Venues() {
   useScrollToTop();
-  return <VenuesPage />;
+  return (
+    <>
+      <Helmet>
+        <title>Explore and Search Venues - Holidaze</title>
+        <meta name="description" content="Discover a wide range of accommodations on Holidaze. Use our search feature to find the perfect venue for your next trip. Book now and enjoy a seamless travel experience!" />
+      </Helmet>
+      <VenuesPage />
+    </>
+  );
 }
 
 export default Venues;
