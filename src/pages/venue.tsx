4e16f59b592bae7a2b7516029bc116c592f6ddf3
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-11-30 09:39:30 +0100

    add dynamic title and description to meta description and title in helmet

--- src/pages/venue.tsx
+++ src/pages/venue.tsx
@@ -1,9 +1,30 @@
 import { useScrollToTop } from "../components/utils";
+import { useState } from "react";
 import SingleVenuePage from "../components/venue";
+import { SingleVenueResponse } from "../components/api/const/interfaces";
+import { Helmet } from "react-helmet";
 
 function Venue() {
   useScrollToTop();
-  return <SingleVenuePage />;
+
+  // States for title and description used for meta title and description
+  const [title, setTitle] = useState("Holidaze");
+  const [description, setDescription] = useState("Discover amazing places to stay at Holidaze.");
+
+  const handleVenueData = (data: SingleVenueResponse) => {
+    setTitle(`${data.data.name} - Holidaze`);
+    setDescription(`${data.data.description} - Holidaze`);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <SingleVenuePage onSendData={handleVenueData} />
+    </>
+  );
 }
 
 export default Venue;
