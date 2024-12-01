bff3d193ef56fcfa8e45da08fc4a1a7dee0a8f29
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-01 16:27:44 +0100

    changes to loader on frontpage

--- src/components/frontpageContent/FrontpageVenuePreview.tsx
+++ src/components/frontpageContent/FrontpageVenuePreview.tsx
@@ -1,20 +1,37 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useApi } from "../api";
 import { Venue, VenueResponse } from "../api/const/interfaces";
 import { VENUES_ENDPOINT } from "../api/const";
-import { FrontPageLoader, FrontPageError } from "./";
+// import { FrontPageLoader, FrontPageError } from "./";
+import { Spinner } from "react-bootstrap";
 
 function FrontpageVenuePreview() {
+  const [venueLoader, setVenueLoader] = useState(false);
+  const [loadingError, setLoadingError] = useState<string | null>(null);
   const { data, error, loading } = useApi<null, VenueResponse>(VENUES_ENDPOINT, "GET", null, false);
 
-  if (loading) return <FrontPageLoader />;
-  if (error) return <FrontPageError />;
+  useEffect(() => {
+    if (loading) {
+      setVenueLoader(true);
+    } else {
+      setVenueLoader(false);
+    }
+    if (error) {
+      setVenueLoader(false);
+      setLoadingError("We encountered a problem while finding venues for you. Please try again later.");
+    } else {
+      setLoadingError(null);
+    }
+  }, [loading, error]);
 
   const venues = data?.data || [];
 
   return (
     <section className="container text-center mt-125px mt-md-4">
       <h2 className="pb-5 py-md-5 secondary-font fs-1-5rem-to-2-5rem">Find your next getaway</h2>
+      {venueLoader && <Spinner />}
+      {loadingError && <p>{loadingError}</p>}
       <div className="row pb-5 gy-4">
         {venues.slice(0, 4).map((venue: Venue) => (
           <div key={venue.id} className="col-6 col-lg-3 fp-img-container">
