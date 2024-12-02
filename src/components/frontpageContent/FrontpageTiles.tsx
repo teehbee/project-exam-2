d7d6c23510f3899cb8d7caf622e31bcd076ab53c
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-02 09:14:10 +0100

    small correction to hover class for frontpage

--- src/components/frontpageContent/FrontpageTiles.tsx
+++ src/components/frontpageContent/FrontpageTiles.tsx
@@ -11,7 +11,7 @@ function FrontpageTiles() {
         <div className="col-12 col-lg-6 text-center text-lg-start d-flex flex-column justify-content-center p-0">
           <h3 className="secondary-font fs-1-5rem-to-2-5rem pt-3 pt-lg-0 px-3">Earn money on your property</h3>
           <p className="p-3">Find your perfect stay with Holidaze! Easily search and book unique accommodations, from apartments to villas, using our simple filters. With secure payments and detailed listings, booking your next getaway is a breeze!</p>
-          <Link to="register" className="font-gray p-3 link-hover">
+          <Link to="register" className="font-gray p-3 link-hover-md">
             Register here!
           </Link>
         </div>
@@ -23,7 +23,7 @@ function FrontpageTiles() {
         <div className="col-12 col-lg-6 text-center text-lg-end d-flex flex-column justify-content-center">
           <h3 className="secondary-font fs-1-5rem-to-2-5rem pt-3 pt-lg-0">Find your next getaway</h3>
           <p className="p-3">Turn your property into a vacation rental with Holidaze! List your space in minutes, set your own prices, and manage bookings with ease. Our secure system ensures hassle-free payments, so you can start earning extra income right away.</p>
-          <Link to="venues" className="font-gray p-3 link-hover">
+          <Link to="venues" className="font-gray p-3 link-hover-md">
             Look for accommodation here
           </Link>
         </div>
