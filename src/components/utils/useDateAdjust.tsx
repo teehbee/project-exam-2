78fd9ca9dd5c95e397be83d38121ec9658b366ec
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-12-05 15:35:49 +0100

    add fallback value to hook for adjusting date

--- src/components/utils/useDateAdjust.tsx
+++ src/components/utils/useDateAdjust.tsx
@@ -1,16 +1,43 @@
 import { useEffect, useState } from "react";
 import { UseFormSetValue } from "react-hook-form";
+import { getTomorrowsDate } from "./dateUtils";
 
-function useDateAdjust(dateFrom: string | undefined, setValue: UseFormSetValue<any>) {
+// function useDateAdjust(dateFrom: string | undefined, setValue: UseFormSetValue<any>) {
+//   const [minDateTo, setMinDateTo] = useState<string>("");
+
+//   useEffect(() => {
+//     if (dateFrom) {
+//       const nextDay = new Date(dateFrom);
+//       nextDay.setDate(nextDay.getDate() + 1);
+//       const newMinDateTo = nextDay.toISOString().split("T")[0];
+//       setMinDateTo(newMinDateTo);
+//       setValue("dateTo", newMinDateTo);
+//     }
+//   }, [dateFrom, setValue]);
+
+//   return minDateTo;
+// }
+
+// export default useDateAdjust;
+
+interface FormData {
+  dateTo: string;
+}
+
+function useDateAdjust(dateFrom: string | undefined, setValue: UseFormSetValue<FormData>) {
   const [minDateTo, setMinDateTo] = useState<string>("");
 
   useEffect(() => {
+    const tomorrow = getTomorrowsDate();
     if (dateFrom) {
       const nextDay = new Date(dateFrom);
       nextDay.setDate(nextDay.getDate() + 1);
       const newMinDateTo = nextDay.toISOString().split("T")[0];
       setMinDateTo(newMinDateTo);
       setValue("dateTo", newMinDateTo);
+    } else {
+      setMinDateTo(tomorrow);
+      setValue("dateTo", tomorrow); // Ensure the fallback value is set in the form
     }
   }, [dateFrom, setValue]);
 
